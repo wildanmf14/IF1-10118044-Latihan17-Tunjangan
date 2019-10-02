6c8ab597355f39b901086645f47065b2ba23bca5
Wildhevire <wildhevire404@gmail.com>
2019-10-02 22:23:24 +0000

    revisi formatting

--- src/latihan/tunjangan/Tunjangan.java
+++ src/latihan/tunjangan/Tunjangan.java
@@ -8,9 +8,9 @@ public class Tunjangan {
         Scanner gajiReader = new Scanner(System.in);
         Scanner statusReader = new Scanner(System.in);
         
-        System.out.print("Berapa gaji pokok Anda perbulan \t: Rp. ");
+        System.out.printf("Berapa gaji pokok Anda perbulan : Rp. ");
         double gajiPokok = gajiReader.nextDouble();
-        System.out.print("Status Anda? (Menikah/Belum) \t: ");
+        System.out.printf("Status Anda? (Menikah/Belum) \t: ");
         String status = statusReader.nextLine();
         double tunjangan,total;
         if(status.equals("Menikah")) 
@@ -18,9 +18,9 @@ public class Tunjangan {
         	tunjangan = gajiPokok * 0.35;
         	total = gajiPokok + tunjangan;
         	System.out.println("==========Hasil Perhitungan Gaji Anda==========");
-        	System.out.printf("Gaji Pokok\t : %,12.2f%n", gajiPokok);
-        	System.out.printf("Tunjangan\t : %,12.2f%n", tunjangan);
-        	System.out.printf("Total Gaji\t : %,12.2f%n", total);
+        	System.out.printf("Gaji Pokok\t : Rp. %,12.2f%n", gajiPokok);
+        	System.out.printf("Tunjangan\t : Rp. %,12.2f%n", tunjangan);
+        	System.out.printf("Total Gaji\t : Rp. %,12.2f%n", total);
         	
         } else {
         	System.out.println("==========Hasil Perhitungan Gaji Anda==========");
